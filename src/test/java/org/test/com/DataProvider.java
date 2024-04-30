1c3165ce140d1bb5e80aba324004a56183edfb65
Ameed.Hudhud <ameedhudhud0@gmail.com>
2024-04-30 15:14:54 +0300

    commit

--- src/test/java/org/test/com/DataProvider.java
+++ src/test/java/org/test/com/DataProvider.java
@@ -1,7 +1,37 @@
 package org.test.com;
+import com.google.gson.Gson;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 public class DataProvider {
+
+     String[] skillData = {"java"};
+     String[] companyData = {"FedEx"};
+     String[] experienceData = {"0 - 1 years"};
+     String[] locationData = {"Chennai"};
+     String[] combinedData = {"java","FedEx","Hyderabad"};
+     String[] remoteData = {"Remote"};
+     String[] invalidCompany = {"testComapny"};
+     String[] validCompany = {"Securonix"};
+     String[] skillWithNoJob = {"noJob"};
+     String[] locationWithNoJob = {"nablus"};
+
+    JSONParser parser = new JSONParser();
+    FileReader reader = new FileReader("src/test/java/org/test/com/data.JSON");
+    Object obj = parser.parse(reader);
+    JSONObject data = (JSONObject) obj;
+    Locators x = new Gson().fromJson(String.valueOf(data), Locators.class);
+
+    public DataProvider() throws IOException, ParseException {
+
+    }
+
+
     @org.testng.annotations.DataProvider
     public Object[][] getData(Method method){
         switch (method.getName()) {
@@ -44,6 +74,50 @@ public class DataProvider {
             case "VerifyThatTheUserIsUnableToChangeThePasswordWithANewPasswordOfLessThan8Characters":
                 return new Object[][] {
                         {"ameed0595", "12345", "12345", "Ensure this field has at least 8 characters."}
+                };
+            case "FilterJobsBySelectingASpecificSkill":
+                return new Object[][] {
+                        {x.getInputFieldSkills(),x.getSearchButton(),x.getFirstJob(),skillData}
+                };
+            case "FilterJobsBySelectingACompanyName":
+                return new Object[][] {
+                        {x.getCompaniesMenu(),x.getFirstJob(),companyData}
+                };
+            case "FilterJobsBySpecifyingARangeOfExperience":
+                return new Object[][] {
+                        {x.getExperienceRangeMenu(),x.getFirstJob(),experienceData[0]}
+                };
+            case "FilterJobsBySelectingAJobLocation":
+                return new Object[][] {
+                        {x.getInputFieldLocation(),x.getSearchButton(),x.getFirstJob(),locationData}
+                };
+            case "VerifyFilteredJobsBySelectingSkillAndCompanyAndNameAndExperienceRangeAndJobLocation":
+                return new Object[][] {
+                        {x.getInputFieldSkills(),x.getCompaniesMenu(),x.getExperienceRangeMenu(),x.getInputFieldLocation(),x.getSearchButton(),x.getFirstJob(),combinedData,experienceData[0]}
+                };
+            case "FilterJobsByChoosingRemoteJobOption":
+                return new Object[][] {
+                        {x.getRemote(),x.getFirstJob(),remoteData}
+                };
+            case "FilterCompaniesByEnteringAValidCompanyName":
+                return new Object[][] {
+                        {x.getCompaniesMenu(),x.getInputFieldCompany(),companyData}
+                };
+            case "VerifyNoCompaniesAreReturnedWhenFilteringByAnInvalidCompanyName":
+                return new Object[][] {
+                        {x.getCompaniesMenu(),x.getInputFieldCompany(),invalidCompany}
+                };
+            case "FilterJobsWithACompanyThatHasNoJobs":
+                return new Object[][] {
+                        {x.getCompaniesMenu(),validCompany,x.getNoJobMessage()}
+                };
+            case "FilterJobsWithASkillThatHasNoJobs":
+                return new Object[][] {
+                        {x.getInputFieldSkills(),x.getSearchButton(),skillWithNoJob,x.getNoJobMessage()}
+                };
+            case "FilterJobsWithAJobLocationThatHasNoJobs":
+                return new Object[][] {
+                        {x.getInputFieldLocation(),x.getSearchButton(),locationWithNoJob,x.getNoJobMessage()}
                 };
             default:
                 return null;
--- src/test/java/org/test/com/DiscoverJobs.java
+++ src/test/java/org/test/com/DiscoverJobs.java
@@ -6,105 +6,89 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-
 public class DiscoverJobs extends DiscoverJobsHelper {
 
     protected static WebDriver driver;
     @BeforeMethod
     public void visitSettingPage(){
         driver = new ChromeDriver();
-//        driver.get("https://talent500.co/auth/signin?redirect_to=jobs");
-//        login("hudhudameed@gmail.com","ameed0595",driver,true);
+        driver.get("https://talent500.co/auth/signin?redirect_to=jobs");
+        login("hudhudameed@gmail.com","ameed0595",driver,true);
         driver.get("https://talent500.co/jobs");
         driver.manage().window().maximize();
 
-//        System.out.println(driver.getPageSource());
     }
-    @Test
-    public void FilterJobsBySelectingASpecificSkill() throws InterruptedException {
-        String[] data = {"java"};
-        enterFieldValue("[placeholder=\"Search by any field (e.g skills, title, etc.)\"]","java",driver);
-        clickButton("Search",driver,true);
-        clickButton("View Job",driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsBySelectingASpecificSkill(String skillsFieldLocator, String searchButtonLocator, String firstJobLocator,  String[] data) throws InterruptedException {
+        enterFieldValue(skillsFieldLocator,data[0],driver);
+        clickButton(searchButtonLocator,driver,true);
+        clickButton(firstJobLocator,driver,false);
         verifyJavaPresence(data,driver,true);
     }
-    @Test
-    public void FilterJobsBySelectingACompanyName() throws InterruptedException {
-        String[] data = {"FedEx"};
-        selectFromMenu("data-id=\"jobs-DDHeaderWrapper\"","FedEx",driver);
-        clickButton("View Job",driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsBySelectingACompanyName(String companiesMenuLocator, String firstJobLocator, String[] data) throws InterruptedException {
+        selectFromMenu(companiesMenuLocator,data[0],driver);
+        clickButton(firstJobLocator,driver,false);
         verifyJavaPresence(data,driver,true);
     }
-    @Test
-    public void FilterJobsBySpecifyingARangeOfExperience() throws InterruptedException {
-        selectFromMenu("data-id=\"DropdownHero\"","0 - 1 years",driver);
-        clickButton("View Job",driver,true);
-        verifyExperience("0 - 1 years");
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsBySpecifyingARangeOfExperience(String experienceRangeMenuLocator, String firstJobLocator, String range) throws InterruptedException {
+        selectFromMenu(experienceRangeMenuLocator,range,driver);
+        clickButton(firstJobLocator,driver,false);
+        verifyExperience(range);
     }
-
-    @Test
-    public void FilterJobsBySelectingAJobLocation() throws InterruptedException {
-        String[] data = {"Chennai"};
-        enterFieldValue("[placeholder=\"Job location\"]","Chennai",driver);
-        clickButton("Search",driver,true);
-        clickButton("View Job",driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsBySelectingAJobLocation(String locationFieldLocator, String searchButtonLocator, String firstJobLocator, String[] data) throws InterruptedException {
+        enterFieldValue(locationFieldLocator,data[0],driver);
+        clickButton(searchButtonLocator,driver,true);
+        clickButton(firstJobLocator,driver,false);
         verifyJavaPresence(data,driver,true);
     }
-    @Test
-    public void VerifyFilteredJobsBySelectingSkillAndCompanyAndNameAndExperienceRangeAndJobLocation() throws InterruptedException {
-        String []information = {"FedEx","Hyderabad","java"};
-        enterFieldValue("[placeholder=\"Search by any field (e.g skills, title, etc.)\"]","java",driver);
-        selectFromMenu("data-id=\"jobs-DDHeaderWrapper\"","FedEx",driver);
-        selectFromMenu("data-id=\"DropdownHero\"","0 - 1 years",driver);
-        enterFieldValue("[placeholder=\"Job location\"]","Hyderabad",driver);
-        clickButton("Search",driver,true);
-        clickButton("View Job",driver,true);
-        verifyJavaPresence(information,driver,true);
-        verifyExperience("0 - 1 years");
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void VerifyFilteredJobsBySelectingSkillAndCompanyAndNameAndExperienceRangeAndJobLocation(String skillsField, String companyMenu, String experienceRangeMenu, String location, String searchButton, String firstJob, String[] data, String range) throws InterruptedException {
+        enterFieldValue(skillsField,data[0],driver);
+        selectFromMenu(companyMenu,data[1],driver);
+        selectFromMenu(experienceRangeMenu,range,driver);
+        enterFieldValue(location,data[2],driver);
+        clickButton(searchButton,driver,true);
+        clickButton(firstJob,driver,false);
+        verifyJavaPresence(data,driver,true);
+        verifyExperience(range);
     }
-    @Test
-    public void FilterJobsByChoosingRemoteJobOption() throws InterruptedException {
-        String []data = {"Remote"};
-        clickButton("Show remote jobs",driver,true);
-        clickButton("View Job",driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsByChoosingRemoteJobOption(String remoteButton, String firstJob, String[] data) throws InterruptedException {
+        clickButton(remoteButton,driver,true);
+        clickButton(firstJob,driver,false);
         verifyJavaPresence(data,driver,true);
     }
-    @Test
-    public void FilterCompaniesByEnteringAValidCompanyName() throws InterruptedException {
-        String[] data = {"FedEx"};
-        clickButton("data-id=jobs-DDHeaderWrapper",driver,false);
-        enterFieldValue("[placeholder=\"Search company\"]","FedEx",driver);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterCompaniesByEnteringAValidCompanyName(String companiesMenuLocator, String companyNameFieldLocator, String[] data) throws InterruptedException {
+        clickButton(companiesMenuLocator,driver,false);
+        enterFieldValue(companyNameFieldLocator,data[0],driver);
         verifyJavaPresence(data,driver,true);
     }
-    @Test
-    public void VerifyNoCompaniesAreReturnedWhenFilteringByAnInvalidCompanyName() throws InterruptedException {
-        String[] data = {"test company"};
-        clickButton("data-id=jobs-DDHeaderWrapper",driver,false);
-        enterFieldValue("[placeholder=\"Search company\"]","test",driver);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void VerifyNoCompaniesAreReturnedWhenFilteringByAnInvalidCompanyName(String companiesMenuLocator, String companyNameFieldLocator, String[] data) throws InterruptedException {
+        clickButton(companiesMenuLocator,driver,false);
+        enterFieldValue(companyNameFieldLocator,data[0],driver);
         verifyJavaPresence(data,driver,false);
     }
-//    Filter jobs with a company that has no jobs
-@Test
-public void FilterJobsWithACompanyThatHasNoJobs() throws InterruptedException {
-    String[] data = {"Ah, no jobs for these filters right now."};
-    selectFromMenu("data-id=\"jobs-DDHeaderWrapper\"","Securonix",driver);
-    verifyJavaPresence(data,driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsWithACompanyThatHasNoJobs(String companiesMenuLocator, String[] data, String[] message) throws InterruptedException {
+    selectFromMenu(companiesMenuLocator,data[0],driver);
+    verifyJavaPresence(message,driver,true);
 }
-    @Test
-    public void FilterJobsWithASkillThatHasNoJobs() throws InterruptedException {
-        String[] data = {"Ah, no jobs for these filters right now."};
-        enterFieldValue("[placeholder=\"Search by any field (e.g skills, title, etc.)\"]","noJob",driver);
-        clickButton("Search",driver,true);
-        verifyJavaPresence(data,driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsWithASkillThatHasNoJobs(String skillsFieldLocator, String searchButtonLocator, String[] data, String[] message) throws InterruptedException {
+        enterFieldValue(skillsFieldLocator,data[0],driver);
+        clickButton(searchButtonLocator,driver,true);
+        verifyJavaPresence(message,driver,true);
     }
-    @Test
-    public void FilterJobsWithAJobLocationThatHasNoJobs() throws InterruptedException {
-        String[] data = {"Ah, no jobs for these filters right now."};
-        enterFieldValue("[placeholder=\"Job location\"]","nablus",driver);
-        clickButton("Search",driver,true);
-        verifyJavaPresence(data,driver,true);
+    @Test(dataProvider = "getData",dataProviderClass= DataProvider.class)
+    public void FilterJobsWithAJobLocationThatHasNoJobs(String locationFieldLocator, String searchButtonLocator, String[] data, String[] message) throws InterruptedException {
+        enterFieldValue(locationFieldLocator,data[0],driver);
+        clickButton(searchButtonLocator,driver,true);
+        verifyJavaPresence(message,driver,true);
     }
     @AfterMethod
     public void quit(){
